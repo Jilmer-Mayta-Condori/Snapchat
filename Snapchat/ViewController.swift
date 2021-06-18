bec0b3a7ec31f2ecab5cb7a535a4eb86f25474be
Jilmer Mayta <jilmer.mayta@tecsup.edu.pe>
2021-06-18 08:34:54 -0500

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Jilmer Mayta on 6/18/21.
+//  Copyright © 2021 Jilmer Mayta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
